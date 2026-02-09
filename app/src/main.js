4ca3a5e3cc6a87e01185c47eff94de604db201af
William <william.jiang1101@gmail.com>
2026-02-09 05:30:28 -0500

    Completed

--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,7 @@ import { renderPokemon, renderError, renderSuccess } from "./dom-helpers";
 import { getRandomPokemon, postDiscoveredPokemon } from "./fetch-helpers";
 const button = document.querySelector('#discover-button')
 const form = document.querySelector('form');
+const allowedPKMN = new Set();
 
 async function getAndRenderPokemon() {
   const pokemon = await getRandomPokemon();
@@ -13,6 +14,7 @@ async function getAndRenderPokemon() {
 
   renderPokemon(pokemon.data);
   renderSuccess(`${pokemon.data.name} was discovered!`);
+  allowedPKMN.add(pokemon.data.name.toLowerCase());
 }
 
 getAndRenderPokemon();
@@ -32,6 +34,9 @@ form.addEventListener(('submit'), async (event) => {
   const data = await postDiscoveredPokemon(formValues);
   if(data.error){
     renderError(`Error: unable to capture Pokémon. Please try again later`);
+    return;
+  } else if (!allowedPKMN.has(formValues.name.toLowerCase())){
+    renderError(`Error: You haven't discovered this pokemon!`);
     return;
   }
   renderSuccess(`${formValues.name} has been captured!`)
